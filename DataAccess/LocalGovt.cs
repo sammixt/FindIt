16bcbe1518960dc94b1b8c4bb905cadb2d09a829
unknown <esc@ESC87A2>
2020-03-05 20:22:33 +0100

    seed created

--- DataAccess/LocalGovt.cs
+++ DataAccess/LocalGovt.cs
@@ -8,6 +8,6 @@ namespace DataAccess
     {
         public int Id { get; set; }
         public int StateId { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
     }
 }
